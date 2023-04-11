f0f35e82d563a7856a232c7b6a615da26f9c8a48
haedawn <junmin0529@kookmin.ac.kr>
2023-04-11 09:49:39 +0900

    haedawn_staticmedia

--- session/blog/views.py
+++ session/blog/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render,get_object_or_404,redirect
+from django.core.paginator import Paginator
 from .models import Blog
 from .forms import BlogForm
 
 
 def home(request):
     blogs = Blog.objects.all()
-    return render(request,'home.html',{'blogs':blogs})
+    paginator = Paginator(blogs, 3)
+    page_number = request.GET.get('page')
+    page_obj= paginator.get_page(page_number)
+    return render(request,'home.html',{'page_obj':page_obj})
 
 def detail(request, blog_id):
     blog = get_object_or_404(Blog, pk=blog_id)
@@ -16,8 +20,11 @@ def new(request):
 
 def create(request):
     new_blog = Blog()
-    new_blog.title = request.POST['title']
-    new_blog.content = request.POST['content']
+    new_blog.title = request.POST.get('title')
+    new_blog.content = request.POST.get('content')
+    print(new_blog.content)
+    new_blog.image = request.FILES.get('image')
+    print(new_blog.image)
     new_blog.save()
     return redirect('detail', new_blog.id)
     # return render(request, 'detail.html', {'blog':new_blog})
@@ -39,8 +46,9 @@ def edit(request, blog_id):
 
 def update(request, blog_id):
     old_blog = get_object_or_404(Blog, pk=blog_id)
-    old_blog.title = request.POST["title"]
-    old_blog.content = request.POST["content"]
+    old_blog.title = request.POST.get('title')
+    old_blog.content = request.POST.get('content')
+    old_blog.image = request.FILES.get('image')
     old_blog.save()
     return redirect('detail', old_blog.id)
 
@@ -49,12 +57,12 @@ def update(request, blog_id):
 #     form = BlogForm(request.POST, instance=old_blog)
 
     # 클라이언트가 유효한 값을 입력한 경우
-    if form.is_valid():
-        new_blog = form.save(commit=False)
-        new_blog.save()
-        return redirect('detail', old_blog.id)
+    # if form.is_valid():
+    #     new_blog = form.save(commit=False)
+    #     new_blog.save()
+    #     return redirect('detail', old_blog.id)
 
-    return render(request, 'new.html', {'old_blog':old_blog})
+    # return render(request, 'new.html', {'old_blog':old_blog})
 
 
 def delete(request, blog_id):
